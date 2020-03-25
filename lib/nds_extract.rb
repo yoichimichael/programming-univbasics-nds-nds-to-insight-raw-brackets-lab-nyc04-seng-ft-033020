41ac2c2896a5583c91402a909874619f6ed18eea
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-25 00:55:50 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+pp directors_database
+
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -18,5 +20,27 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  director_gross_earnings = {}
+  director_counter = 0 
+  while director_counter < directors_database.length do
+    movie_counter = 0
+    
+    #below might be unecessary variable, assigned for clarity in following while loop
+    
+    num_of_movies = directors_database[director_counter][:movies].length
+    total_gross = 0
+    
+    while movie_counter < num_of_movies do
+      movie_gross = directors_database[director_counter][:movies][movie_counter][:worldwide_gross]
+      total_gross += movie_gross
+      movie_counter += 1
+    end
+    
+    #below variable also assigned for clarity
+    
+    director = directors_database[director_counter][:name]
+    director_gross_earnings[director] = total_gross
+    director_counter += 1  
+  end
+  director_gross_earnings
 end
